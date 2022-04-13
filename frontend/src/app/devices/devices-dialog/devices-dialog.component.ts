a12e12ee6d5c73f56bf6d2ab7e5d0175be14d156
Alessandro <sandromuniz27@hotmail.com>
2022-04-13 17:49:32 -0400

    add pages devices

---
+++ frontend/src/app/devices/devices-dialog/devices-dialog.component.ts
@@ -0,0 +1,50 @@
+import { Component, EventEmitter, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { CategoryModel } from '../../models/category';
+import { DeviceModel } from '../../models/device';
+
+import { ServicesCategory } from '../../services/services-category';
+
+@Component({
+  selector: 'devices-dialog',
+  templateUrl: './devices-dialog.component.html',
+  styleUrls: ['./devices-dialog.component.css'],
+})
+export class DevicesDialogComponent implements OnInit {
+  params = {
+    id: 0,
+    categoryId: 0,
+    color: '',
+    partNumber: 0,
+  };
+  public event: EventEmitter<any> = new EventEmitter();
+
+  constructor(
+    public dialogRef: MatDialogRef<DevicesDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DeviceModel,
+    public _services: ServicesCategory
+  ) {
+    this.params.id = data.id;
+    this.params.categoryId = data.categoryId;
+    this.params.color = data.color;
+    this.params.partNumber = data.partNumber;
+  }
+
+  public categories: CategoryModel[] = [];
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onSubmit(): void {
+    this.event.emit(this.params);
+    this.dialogRef.close();
+  }
+
+  ngOnInit(): void {
+    this._services.getCategories().subscribe((res) => {
+      this.categories = res;
+    });
+  }
+}
---
+++ frontend/src/app/devices/devices.component.ts
@@ -0,0 +1,127 @@
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { Subscription } from 'rxjs';
+
+import { DevicesDialogComponent } from './devices-dialog/devices-dialog.component';
+
+import { ServicesDevice } from '../services/services-device';
+
+import { DeviceModel } from '../models/device';
+
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-devices',
+  templateUrl: './devices.component.html',
+  styleUrls: ['./devices.component.css'],
+})
+export class DevicesComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  displayedColumns = ['id', 'category', 'color', 'partNumber', 'actions'];
+
+  public dataSource: MatTableDataSource<DeviceModel>;
+  private serviceSubscribe: Subscription;
+
+  constructor(public _dialog: MatDialog, private _services: ServicesDevice) {
+    this.dataSource = new MatTableDataSource<DeviceModel>();
+    this.serviceSubscribe = this._services.devices$.subscribe((res) => {
+      this.dataSource.data = res;
+    });
+  }
+
+  ngOnInit() {
+    this.get();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  ngOnDestroy(): void {
+    this.serviceSubscribe.unsubscribe();
+  }
+
+  get() {
+    this._services.getDevices().subscribe(
+      (res) => {
+        this.dataSource.data = res;
+        if (this.dataSource.data.length === 0)
+          Swal.fire('Warning', 'Device Not Found!', 'warning');
+      },
+      (err) => {
+        Swal.fire('Ops!', err.message, 'error');
+      }
+    );
+  }
+
+  openDialog(item?: DeviceModel): void {
+    let dialogRef = this._dialog.open(DevicesDialogComponent, {
+      width: '600px',
+      data: {
+        id: item == null ? 0 : item.id,
+        categoryId: item == null ? 0 : item.categoryId,
+        color: item == null ? '' : item.color,
+        partNumber: item == null ? 0 : item.partNumber,
+      },
+    });
+    dialogRef.componentInstance.event.subscribe((result) => {
+      Swal.fire({
+        title: 'Attention',
+        text: 'Do you confirm?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: 'green',
+        cancelButtonColor: 'red',
+        confirmButtonText: 'Yes, save!',
+        cancelButtonText: 'No, do not save!',
+      }).then((res) => {
+        if (res.isConfirmed)
+          this._services.saveDevice(result).subscribe(
+            (res) => {
+              Swal.fire('Success!', 'Device was saved.', 'success');
+              this.get();
+            },
+            (err) => {
+              Swal.fire('Ops!', err.message, 'error');
+            }
+          );
+      });
+    });
+  }
+
+  delete(item: DeviceModel) {
+    Swal.fire({
+      title: 'Warning',
+      text: 'Do you confirm?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: 'green',
+      cancelButtonColor: 'red',
+      confirmButtonText: 'Yes, delete!',
+      cancelButtonText: 'No, do not delete!',
+    }).then((res) => {
+      if (res.isConfirmed)
+        this._services.deleteDevice(item.id).subscribe(
+          (res) => {
+            Swal.fire('Success!', 'Device was deleted.', 'success');
+            this.get();
+          },
+          (err) => {
+            Swal.fire('Ops!', err.message, 'error');
+          }
+        );
+    });
+  }
+}

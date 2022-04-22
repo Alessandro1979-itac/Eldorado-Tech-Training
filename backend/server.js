97eb6ba413bd45de596352a5dc4131939d4be09d
Alessandro <sandromuniz27@hotmail.com>
2022-04-22 16:57:23 -0400

    add rotas usuarios

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const app = require('./config/express')();
-const devices_routes = require('./api/routes/devices.js');
-const categories_routes = require('./api/routes/categories.js');
+const devices_routes = require('./api/routes/devices');
+const categories_routes = require('./api/routes/categories');
+const usuarios_routes = require('./api/routes/usuarios');
 
 const port = app.get('port');
 
 app.use('/api/device', devices_routes);
 app.use('/api/category', categories_routes);
+app.use('/api/usuario', usuarios_routes);
 
 // RODANDO NOSSA APLICAÇÃO NA PORTA SETADA
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  // console.log(`Servidor rodando na porta ${port}`);
+  console.log(`⚡[start]:🚀 ${process.env.BASE}${process.env.PORT}`);
 });

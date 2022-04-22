0e6d328e3cebd80bad00d4592dcdac2d6d0ce9a1
Alessandro <sandromuniz27@hotmail.com>
2022-04-22 16:53:12 -0400

    add middleware

---
+++ backend/middleware/login.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const required = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decode = jwt.verify(token, process.env.JWT_KEY);
+    req.user = decode;
+    next();
+  } catch (error) {
+    return res.status(401).send({ mensagem: 'Falha na autenticação' });
+  }
+};
+
+const optional = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decode = jwt.verify(token, process.env.JWT_KEY);
+    req.user = decode;
+    next();
+  } catch (error) {
+    next();
+  }
+};
+
+module.exports = { required, optional };

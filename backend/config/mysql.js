6eed60e6b09789760da3659ddec23c9d892d4a25
Alessandro <sandromuniz27@hotmail.com>
2022-04-24 20:43:19 -0400

    ajuste no banco de dados

--- backend/config/mysql.js
+++ backend/config/mysql.js
@@ -1,9 +1,26 @@
-module.exports = {
-  client: 'mysql2',
-  connection: {
-    host: process.env.HOST,
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-  },
-};
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT_DB,
+});
+
+connection.connect((error) => {
+  if (error) console.log(`Erro ao tentar conectar no MySQL ${error}`);
+  else console.log('Conectado ao MySQL');
+});
+
+module.exports = connection;
+
+// module.exports = {
+//   client: 'mysql2',
+//   connection: {
+//     host: process.env.HOST,
+//     user: process.env.USERNAME,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE,
+//   },
+// };

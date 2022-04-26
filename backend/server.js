a65aec82d99248e30f2df15c4182cae52b5d5f93
Alessandro <sandromuniz27@hotmail.com>
2022-04-26 12:01:37 -0400

    retirada de comentarios

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,3 @@
-// const app = require('./config/express')();
-// const devices_routes = require('./api/routes/devices');
-// const categories_routes = require('./api/routes/categories');
-// const usuarios_routes = require('./api/routes/usuarios');
-
-// const port = app.get('port');
-
-// app.use('/api/device', devices_routes);
-// app.use('/api/category', categories_routes);
-// app.use('/api/users', usuarios_routes);
-
-// // RODANDO NOSSA APLICAÇÃO NA PORTA SETADA
-// app.listen(port, () => {
-//   // console.log(`Servidor rodando na porta ${port}`);
-//   console.log(`⚡[start]:🚀 ${process.env.BASE}${process.env.PORT}`);
-// });
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
